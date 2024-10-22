9e812fb45c6b3208bf037de49e9f561d30c08287
Albert Lanza <albert2000_lanza@hotmail.com>
2024-10-23 00:54:13 +0200

    Change url Cloudinary images

--- src/data/gameProps.ts
+++ src/data/gameProps.ts
@@ -6,31 +6,31 @@ export function gameProps(lang: PageLanguages, character: Character) {
 
   const gamePropsInfo: Record<Character, GameProps> = {
     Midudev: {
-      imgStringId: 'p4ycfe6jpiddv269ercf',
+      imgStringId: 'a7ir9cdkonglcrdpgvxm',
       positionAnswers: { x: 1108.885342744734, y: 191.29497813100062 },
       miduHelpTxt: t('game.miduHelpTxt.midu'),
       surrenderTxt: t('game.surrenderTxt.midu'),
     },
     Altman: {
-      imgStringId: 'x2k5fyp96c3il7tevhgb',
+      imgStringId: 'sq2w25l3wfasuugumvof',
       positionAnswers: { x: 553.5874517882188, y: 904.2516668444634 },
       miduHelpTxt: t('game.miduHelpTxt.altman'),
       surrenderTxt: t('game.surrenderTxt.altman'),
     },
     PHP: {
-      imgStringId: 'igsclbcucvcrlwurlnye',
+      imgStringId: 'lojix6uftxjahypobl3s',
       positionAnswers: { x: 535.5236048419966, y: 440.41467089822913 },
       miduHelpTxt: t('game.miduHelpTxt.php'),
       surrenderTxt: t('game.surrenderTxt.php'),
     },
     Lady_Gaga: {
-      imgStringId: 'gadpdqbnltidlmmpqpf3',
+      imgStringId: 'seypivyc7825n9vzqoa6',
       positionAnswers: { x: 82.84699897315167, y: 701.7238238745466 },
       miduHelpTxt: t('game.miduHelpTxt.gaga'),
       surrenderTxt: t('game.surrenderTxt.gaga'),
     },
     Colby: {
-      imgStringId: 'ebhegso1cpnhbjxitxvh',
+      imgStringId: 'nx9shoem6shadf3d1rds',
       positionAnswers: { x: 1036.2567134537428, y: 870.600790146545 },
       miduHelpTxt: t('game.miduHelpTxt.colby'),
       surrenderTxt: t('game.surrenderTxt.colby'),
